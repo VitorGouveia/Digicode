5fdc7f11eb749d637425a4be1a4061e5664698c0
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-25 21:56:01 -0300

    feat: creates basic sorting algorithm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,54 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'atlas';
 
-  result = ""
   before = ""
   after = ""
-  getResult(number: string) {
-    this.result = eval(number)
+  result = ""
+  getResult(before: string, after: string) {
+    this.result = `${before} = ${after}`
+  }
+
+  splitVariables(values: string) {
+    const wordsArray = values.split(" ")
+    let final = ""
+    wordsArray.forEach(word => {
+      if (word.includes("x") === true) {
+        const currentIdx = wordsArray.indexOf(word);
+        const hasPrevious = wordsArray[currentIdx - 1];
+
+        if (!!hasPrevious === false) {
+          final = `${wordsArray[currentIdx]}`;
+        } else {
+          final = `${final} ${hasPrevious} ${wordsArray[currentIdx]}`;
+        }
+      }
+    })
+
+    return final
   }
 
   fetchBefore(newData: string) {
-    this.before = newData
-    console.log(newData)
-    this.getResult(this.before)
+    const final = this.splitVariables(newData)
+    newData.split(' ').forEach(word => {
+      const isSign = word.includes("+") || word.includes("-")
+      if(word.includes("x") !== true && isSign === false) {
+        // if index is 0 and is sign
+        const currentIdx = newData.split(' ').indexOf(word)
+        const hasPrevious = newData.split(' ')[currentIdx - 1]
+        if (!!hasPrevious === false && isSign === false) {
+          this.before = `${newData.split(' ')[currentIdx]}`;
+        } else {
+          this.before = `${this.before} ${hasPrevious} ${newData.split(' ')[currentIdx]}`;
+        }
+      }
+    })
+
+    this.getResult(this.before, final)
   }
 
   fetchAfter(newData: string) {
     this.after = newData
-    this.getResult(this.after)
+    const final = this.splitVariables(this.before)
+    this.getResult(this.before, final)
   }
 }
